90f07543fc362587ff67834c35885bb4709f66df
Hacker-Catt <150083406+Hacker-Catt@users.noreply.github.com>
2023-11-14 16:38:56 +0100

    Update curlScript.sh
    Update so https;// is deleted from the URL list

--- curlScript.sh
+++ curlScript.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-input_file="scriptInput.txt"   # Replace with the path to your input file
-output_file="curlResults.txt"    # Replace with the desired output file
+input_file="inputFile.txt"   # Replace with the path to your input file
+output_file="output.txt"    # Replace with the desired output file
 
 while IFS= read -r url; do
-    echo "URL: $url" >> "$output_file"
+    url_without_protocol="${url#https://}"  # Remove 'https://' prefix
+    echo "URL: $url_without_protocol" >> "$output_file"
     echo "Response:" >> "$output_file"
-    curl -L "$url" >> "$output_file"  # Use -L option if you want to follow redirects
-    echo -e "\n=================================================\n" >> "$output_file"
+    curl -L "https://$url_without_protocol" >> "$output_file"  # Use -L option if you want to fo>    echo -e "\n=================================================\n" >> "$output_file"
 done < "$input_file"
 
 echo "Script executed successfully. Check '$output_file' for the responses."
 
+# Script deletes the https:// part from url and runs it through curl -L
+# Edited as Https:// gives issues
+
 # Credits to HackerCatt
-# File created to check if URLs give CURL -L response. input_file need to be the file with the URLs you want to check. output_file gets created after run.
